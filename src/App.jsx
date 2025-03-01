219b53c25e4d8bbf48e074c72892ffe68621d85d
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-03-01 13:04:58 +0330

    Creating the initial project structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="container bg-red-600 mx-auto">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
